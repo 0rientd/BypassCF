0352293f02ff0723542ccc0c4a6e4a7fa0ce9f9a
root <root@carlos-Elementary.(none)>
2015-03-15 17:14:51 -0300

    Lançada a versão 1.2 do BypassCF com uma melhor aparencia com o GeoIP

--- BypassCF.pl
+++ BypassCF.pl
@@ -17,15 +17,15 @@
 
 use strict;
 use warnings;
-use WWW::Mechanize;
 use utf8;
 use Getopt::Long;
+use WWW::Mechanize;
 
 binmode(STDOUT, ":utf8"); # CODE PARA SAÍDA utf8
 
 system 'clear'; # LIMPA A TELA
 
-my ( $baner, $opt, $info, $url, $post, $site, $file, $conteudo, $ipreal ); # DECLARAÇÃO DE VIARIÁVEIS 
+my ( $baner, $opt, $info, $url, $post, $site, $file, $conteudo, $ipreal, $pais, $regiao, $cidade ); # DECLARAÇÃO DE VIARIÁVEIS 
 
 $post = WWW::Mechanize->new(); # INICIO DO Mechanize
 $url = "http://www.crimeflare.com/cfs.html"; # SITE PARA FUTURO "get"
@@ -42,7 +42,7 @@ $baner = "
 ██║     ██║██╔══╝  ██║╚██╗██║██║     ██║██╔══██║    ██╔══██║██╔══██║██║     ██╔═██╗ ██╔══╝  ██╔══██╗                                   
 ╚██████╗██║███████╗██║ ╚████║╚██████╗██║██║  ██║    ██║  ██║██║  ██║╚██████╗██║  ██╗███████╗██║  ██║                                   
  ╚═════╝╚═╝╚══════╝╚═╝  ╚═══╝ ╚═════╝╚═╝╚═╝  ╚═╝    ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝                                   
-                                                                                                    v1.1";
+                                                                                                    v1.2";
 
 $opt = GetOptions ( # CODE PARA ARGUMENTO "-info"
     'info'     => \$info,
@@ -100,31 +100,70 @@ open FILE, ">CFBypass.txt"; # CRIAÇÃO DO ARQUIVO DE TEXTO
 print FILE $file; # COLAGEM DO RESULTADO DA PESQUISA NO SITE NO ARQUIVO TXT 
 close FILE; # FECHAMENTO DO ARQUIVO TXT
 
-if ( $post -> content =~ /aborted/ ) {
+if ( $post -> content =~ /aborted/ ) { # REGEX PARA SABER SE DEU ERRADO
     print "Provavelmente o servidor não se utiliza dos serviços do CloudFlare";
     print "\n";
-} elsif ( $post -> content =~ /Sorry, but the domain name is too short/ ) {
+} elsif ( $post -> content =~ /Sorry, but the domain name is too short/ ) { # REGEX PARA SABER SE O LINK ESTA ERRADO
     print "Ops, algo deu errado...\n";
     print "Verifique o arquivo CFBypass.txt para saber mais sobre o erro\n";
     exit (0);
 } else {
-    open (ARQ, "<CFBypass.txt") or die "Nao foi possivel abrir o arquivo :/";
+    open (ARQ, "<CFBypass.txt") or die "Nao foi possivel abrir o arquivo :/"; # COLAGEM DO CONTEÚDO DO ARQUIVO .txt NA VARIÁVEL
     $conteudo = <ARQ>;
 
     print "----------------------------\n";
     print "Parece que foi encontrado o verdadeiro IP do site :D\n\n";
 
-    if ( $conteudo =~ /(\d{1,9}\.\d{1,9}\.\d{1,9}\.\d{1,9})/ ) {
+    if ( $conteudo =~ /(\d*\.\d*\.\d*\.\d*)/ ) { # REGEX PARA PEGAR SOMENTE O IP REAL DO SITE
         print "[\!] IP real do IP/HOST $site: $1 [\!]";
     }
         
     print "\n\n";
     print "Será criado um arquivo .txt que terá mais informações sobre o servidor\n";
     print "Será também criado um arquivo contendo as informações sobre a localização do IP/HOST\n\n";
+    print "----------------------------\n";
+    print "[\!] GEO LOCALIZAÇÃO [\!]\n";
 
     $post->get ("http://www.telize.com/geoip/".$1);
-    $conteudo = $post->content(format => 'text');
+    $conteudo = $post->content(format => 'text'); 
 
+    if ( $conteudo =~ /("country":"[A-za-z]*"|"country":"[A-za-z]* [A-za-z]*"|"country":"[A-za-z]* [A-za-z]* [A-za-z]*")/ ) { # REGEX PARA PEGAR SOMENTE O PÁIS
+        $pais = $1;
+        if ( $pais =~ /("country")/ ) {
+            $pais =~ s/"country"/País/;
+            print "\n";
+            print "$pais\n";
+        }    
+    } else {
+        print "\n";
+        print "[\!] Desculpe mas não foi possível pegar o país de origem\n";
+        print "Procure no arquivo de texto que será gerado no mesmo local em que o script se encontra [\!]\n\n";
+    }
+
+    if ( $conteudo =~ /("region":"[A-za-z]*|"region":"[A-za-z]* [A-za-z]*")/ ) { # REGEX PARA PEGAR SOMENTE A REGIÃO 
+        $regiao = $1;
+        if ( $regiao =~ /("region")/ ) {
+            $regiao =~ s/"region"/Região/;
+            print "$regiao\n";
+        }
+    } else {
+        print "[\!] Desculpe mas não foi possível pegar o país de origem\n";
+        print "Procure no arquivo de texto que será gerado no mesmo local em que o script se encontra [\!]\n\n";
+    }
+
+    if ( $conteudo =~ /("city":"[A-za-z]*"|"city":"[A-za-z]* [A-za-z]*"|"city":"[A-za-z]* [A-za-z]* [A-za-z]*")/ ) { # REGEX PARA PEGAR SOMENTE A CIDADE
+        $cidade = $1;
+        if ( $cidade =~ /("city")/ ) {
+            $cidade =~ s/"city"/Cidade/;
+            print "$cidade\n";
+        }
+    } else {
+        print "[\!] Desculpe mas não foi possível pegar o país de origem\n";
+        print "Procure no arquivo de texto que será gerado no mesmo local em que o script se encontra [\!]\n\n";
+    }
+    
+    print "\n";
+    print "Qualquer bug, erro, ou dúvida me avise no e-mail hackerorientado\@gmail.com\n";
     open (ARQ, ">GeoIP-Bypass.txt") or die "Não foi possível criar o arquivo txt\n";
     print ARQ $conteudo;
     close ARQ;
